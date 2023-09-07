78401291a302a481fb118aa21c2472ee82045ede
unknown <ankit4b@gmail..com>
2023-09-07 23:36:50 +0530

    images url updated

--- src/app/components/tea-cards/tea-cards.component.ts
+++ src/app/components/tea-cards/tea-cards.component.ts
@@ -8,17 +8,17 @@ import { Component } from '@angular/core';
 export class TeaCardsComponent {
   data:any = [
     {
-      "image" : "../../../assets/images/Group 921.png",
+      "image" : "assets/images/Group 921.png",
       "title" : "Green Tea",
       "description" : "Each of our tea sachets hold generous portions of full leaf tea. Drink a cup of ultra-premium tea with convenience on the go!"
     },
     {
-      "image" : "../../../assets/images/Group 922.png",
+      "image" : "assets/images/Group 922.png",
       "title" : "Matcha Tea",
       "description" : "Enjoy our matcha by sipping the natural boost of antioxidants. We offer a variety of options from hot, cold, whipped or classic."
     },
     {
-      "image" : "../../../assets/images/Group 923.png",
+      "image" : "assets/images/Group 923.png",
       "title" : "Bubble Tea ",
       "description" : "Bubble tea, also known as boba tea, is a tea-based drink flavored with milk or fruit. Our come in variety of vanilla, chocolate and diffrent fruit flavors."
     }
